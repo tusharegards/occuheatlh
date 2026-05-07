78c9547f0c991f414758d640edb37c30e7e55251
harsht261 <harshty261@gmail.com>
2026-05-07 22:28:33 +0530

    in service page update

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
+const REDIRECT_URL = "https://occuhealth.net";
+const navigationEntry = performance.getEntriesByType("navigation")[0] as PerformanceNavigationTiming | undefined;
+const isReload =
+  navigationEntry?.type === "reload" ||
+  (!navigationEntry && performance.navigation.type === 1);
+
+if (isReload && window.location.href !== REDIRECT_URL) {
+  window.location.replace(REDIRECT_URL);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>  
